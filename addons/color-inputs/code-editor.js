e05d77cce6f3d11d69395ca50308cce3d2becb04
Maximouse <mxmou.mouse@gmail.com>
2026-08-07 14:34:06 +0200

    color-inputs in code editor

---
+++ addons/color-inputs/code-editor.js
@@ -0,0 +1,42 @@
+export default async function ({ addon, console }) {
+  const Blockly = await addon.tab.traps.getBlockly();
+
+  const oldCreateLabelDom = Blockly.FieldColourSlider.prototype.createLabelDom_;
+  Blockly.FieldColourSlider.prototype.createLabelDom_ = function (...args) {
+    const [header, readout] = oldCreateLabelDom.call(this, ...args);
+    header.classList.add("sa-color-inputs-row-header");
+    readout.remove();
+    const input = document.createElement("input");
+    input.type = "number";
+    input.min = 0;
+    input.max = 100;
+    input.className = addon.tab.scratchClass("input_input-form", "input_input-small", "input_input-small-range", {
+      others: "sa-color-input",
+    });
+    Object.defineProperty(input, "textContent", {
+      set(newValue) {
+        input.value = newValue;
+      },
+    });
+    header.appendChild(input);
+    return [header, input];
+  }
+
+  const getInputListener = (field, channel) => {
+    const sliderListener = field.sliderCallbackFactory_(channel);
+    return (e) => {
+      let oldValue = e.target.value;
+      e.target.value *= { hue: 360 / 100, saturation: 1 / 100, brightness: 255 / 100 }[channel];
+      sliderListener(e);
+      e.target.value = oldValue;
+    }
+  };
+
+  const oldShowEditor = Blockly.FieldColourSlider.prototype.showEditor_;
+  Blockly.FieldColourSlider.prototype.showEditor_ = function (...args) {
+    oldShowEditor.call(this);
+    this.hueReadout_.addEventListener("input", getInputListener(this, "hue"));
+    this.saturationReadout_.addEventListener("input", getInputListener(this, "saturation"));
+    this.brightnessReadout_.addEventListener("input", getInputListener(this, "brightness"));
+  }
+}
