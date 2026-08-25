f418b929a180c6cce7c5dbc933a09b26474fa7cb
mxmou <mxmou@users.noreply.github.com>
2026-08-25 19:02:25 +0000

    Format code

--- addons/color-inputs/code-editor.js
+++ addons/color-inputs/code-editor.js
@@ -18,7 +18,7 @@ export default async function ({ addon, console }) {
     header.appendChild(input);
     readout.saInput = input;
     return [header, readout];
-  }
+  };
 
   const getInputListener = (field, channel) => {
     const sliderListener = field.sliderCallbackFactory_(channel);
@@ -27,32 +27,24 @@ export default async function ({ addon, console }) {
       e.target.value *= { hue: 360 / 100, saturation: 1 / 100, brightness: 255 / 100 }[channel];
       sliderListener(e);
       e.target.value = oldValue;
-    }
+    };
   };
 
   const oldShowEditor = Blockly.FieldColourSlider.prototype.showEditor_;
   Blockly.FieldColourSlider.prototype.showEditor_ = function (...args) {
     oldShowEditor.call(this, ...args);
-     if (
-      !this.hueReadout_?.saInput ||
-      !this.saturationReadout_?.saInput ||
-      !this.brightnessReadout_?.saInput
-    ) return;
+    if (!this.hueReadout_?.saInput || !this.saturationReadout_?.saInput || !this.brightnessReadout_?.saInput) return;
     this.hueReadout_.saInput.addEventListener("input", getInputListener(this, "hue"));
     this.saturationReadout_.saInput.addEventListener("input", getInputListener(this, "saturation"));
     this.brightnessReadout_.saInput.addEventListener("input", getInputListener(this, "brightness"));
-  }
+  };
 
   const oldUpdateDom = Blockly.FieldColourSlider.prototype.updateDom_;
   Blockly.FieldColourSlider.prototype.updateDom_ = function (...args) {
     oldUpdateDom.call(this, ...args);
-    if (
-      !this.hueReadout_?.saInput ||
-      !this.saturationReadout_?.saInput ||
-      !this.brightnessReadout_?.saInput
-    ) return;
+    if (!this.hueReadout_?.saInput || !this.saturationReadout_?.saInput || !this.brightnessReadout_?.saInput) return;
     this.hueReadout_.saInput.value = roundValue((100 * this.hue_) / 360);
     this.saturationReadout_.saInput.value = roundValue(100 * this.saturation_);
     this.brightnessReadout_.saInput.value = roundValue((100 * this.brightness_) / 255);
-  }
+  };
 }
